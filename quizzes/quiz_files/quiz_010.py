0a7ac0257126cf85047b34b0cfa7099f7b5c7820
May <2025.may.fujita@uwcisak.jp>
2023-09-15 09:53:49 +0900

    modified quiz 010

--- quizzes/quiz_files/quiz_010.py
+++ quizzes/quiz_files/quiz_010.py
@@ -16,18 +16,19 @@
 
 def bestmonthHL(month: int) -> str:
     weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    month_length = {1: [31,'Sun'],
-                   2: [28, 'Wed'],
-                   3: [31, 'Wed'],
-                   4: [30, 'Sat'],
-                   5: [31, 'Mon'],
-                   6: [30, 'Thu'],
-                   7: [31, 'Sat'],
-                   8: [31, 'Tue'],
-                   9: [30, 'Fri'],
-                   10: [31, 'Sun'],
-                   11: [30, 'Wed'],
-                   12: [31, 'Fri']}
+    month_length = {
+        1: [31,'Sun'],
+        2: [28, 'Wed'],
+        3: [31, 'Wed'],
+        4: [30, 'Sat'],
+        5: [31, 'Mon'],
+        6: [30, 'Thu'],
+        7: [31, 'Sat'],
+        8: [31, 'Tue'],
+        9: [30, 'Fri'],
+        10: [31, 'Sun'],
+        11: [30, 'Wed'],
+        12: [31, 'Fri']}
     start_week = month_length[month][1]
     index_week = weekdays.index(start_week)
     output = ''
@@ -41,7 +42,7 @@ def bestmonthHL(month: int) -> str:
     return output
 
 
-print(bestmonthHL(month=5))
+print(bestmonthHL(month=10))
 
 
 # ######Class Solution########
