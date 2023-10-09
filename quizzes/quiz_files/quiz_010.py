f8a92c0c4d80c815e31c55863c87c808848f9c59
May <2025.may.fujita@uwcisak.jp>
2023-10-09 22:21:09 +0900

    Updated quiz documentation

--- quizzes/quiz_files/quiz_010.py
+++ quizzes/quiz_files/quiz_010.py
@@ -11,44 +11,35 @@
 #
 #     return output
 #
-#
 # print(bestMonth())
 
 def bestmonth(month: int) -> str:
-    weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     month_length = {
-        1: [31,'Sun'],
-        2: [28, 'Wed'],
-        3: [31, 'Wed'],
-        4: [30, 'Sat'],
-        5: [31, 'Mon'],
-        6: [30, 'Thu'],
-        7: [31, 'Sat'],
-        8: [31, 'Tue'],
-        9: [30, 'Fri'],
-        10: [31, 'Sun'],
-        11: [30, 'Wed'],
-        12: [31, 'Fri']}
-    start_week = month_length[month][1]
-    index_week = weekdays.index(start_week)
-    output = ''
-    output += f'2023 {months[month-1]}'.center(50) + '\n'
+        1: [31, 6],
+        2: [28, 2],
+        3: [31, 2],
+        4: [30, 5],
+        5: [31, 0],
+        6: [30, 3],
+        7: [31, 5],
+        8: [31, 1],
+        9: [30, 4],
+        10: [31, 6],
+        11: [30, 2],
+        12: [31, 4]}
+    index_week = month_length[month][1]
+    output = f'{("2023 "+months[month-1]).center(20)}\nMo Tu We Th Fr Sa Su\n{(index_week*3)*" "}'
     for n in range(1, month_length[month][0]+1):
         if index_week % 7 == 0:
             index_week = 0
             output += '\n'
-        output += f'{weekdays[index_week]} {n:02d} '
+        output += f'{n:02d} '
         index_week += 1
-
     return output
 
 # Check that it works:
 print(bestmonth(month=10))
-
-#print(f'{months[month-1]}'.lcenter(20))
-#for wd in range(len(weekdays))
-#print(f'{}')
 
 
 # ######Class Solution########
